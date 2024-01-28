793798a3ddb9175be654499e47e66f8cba069e93
jalol <jaloliddinqovlonov15@gmail.com>
2024-01-28 20:08:36 +0200

    UniversityRatings frontend

--- Controllers/ProfessorRatingsController.cs
+++ Controllers/ProfessorRatingsController.cs
@@ -50,7 +50,13 @@ namespace MyRateApp2.Controllers
         // GET: ProfessorRatings/Create
         public IActionResult Create()
         {
-            ViewData["ProfId"] = new SelectList(_context.Professor, "ProfId", "Fname");
+            // Create a new SelectList with both Fname and Lname concatenated
+            ViewData["ProfId"] = new SelectList(_context.Professor.Select(p => new
+            {
+                ProfId = p.ProfId,
+                FullName = p.Fname + " " + p.Lname
+            }), "ProfId", "FullName");
+
             return View();
         }
 
